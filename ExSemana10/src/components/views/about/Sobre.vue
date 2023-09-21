9f835692d035ed7a6224bac3f1680a901edd0b58
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-09-21 01:03:11 -0300

    Criação da componente Sobre
    Criação da componente Sobre

---
+++ ExSemana10/src/components/views/about/Sobre.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <img src="../assets/home.png">
+                <h2>Home</h2>
+            </li>
+            <li>
+                <img src="../assets/folder.png">
+                <h2>Talentos</h2>
+            </li>
+            <li>
+                <img src="../assets/location.png">
+                <h2>Sobre</h2>
+            </li>
+        </ul>
+    </nav>
+
+    <div>
+        <h1>Sobre</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Aenean at lorem a eros rhoncus elementum vel quis ex. Etiam sagittis id nibh at tincidunt. 
+            Mauris laoreet tortor erat, sagittis lacinia ligula sagittis sed. Sed vitae quam eu nisi porta suscipit. 
+            Nunc ac odio in sem iaculis molestie. Fusce cursus ligula libero, eu ultricies dui vehicula vel. 
+            Proin sollicitudin consectetur luctus. Sed sit amet odio purus. Proin posuere felis et nisi aliquam, 
+            eu suscipit diam accumsan. Integer lacus justo, cursus non vehicula nec, sollicitudin a metus. 
+            Donec nunc neque, accumsan eget arcu imperdiet, pellentesque feugiat est. 
+            Aliquam varius, tellus eu pharetra dictum, sem lectus facilisis turpis, quis semper magna nulla vitae purus. 
+            Vivamus commodo id ex quis accumsan. Nam sagittis leo et sem tristique porttitor vel nec lectus. 
+            Vestibulum ante elit, vestibulum at mauris interdum, interdum sodales sapien.</p>
+    </div>
+</template>
+
+<style>
+*{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+
+nav{
+    display: flex;
+    flex-direction: column;
+}
+
+li{
+    border-color: grey;
+}
+
+p {
+    text-align: left;
+    font-size: 12px;
+}
+
+</style>
