58ee3672cc6a07b584ab294c663645068f742cd5
Guimariane <ingrid.mariane.araujo@gmail.com>
2023-09-21 00:40:38 -0300

    Construção da Tela Home
    Construção da Tela Home

---
+++ ExSemana10/src/components/views/home/Home.vue
@@ -0,0 +1,42 @@
+<template>
+<nav>
+    <ul>
+        <li>
+            <img src="../assets/home.png">
+            <h2>Home</h2>
+        </li>
+        <li>
+            <img src="../assets/folder.png">
+            <h2>Talentos</h2>
+        </li>
+        <li>
+            <img src="../assets/location.png">
+            <h2>Sobre</h2>
+        </li>
+    </ul>
+</nav>
+
+<div>
+    <h1>Banco de Talentos</h1>
+</div>
+</template>
+
+<style>
+*{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+
+}
+
+nav{
+    display: flex;
+    flex-direction: column;
+}
+
+li{
+    border-color: grey;
+}
+
+h1{
+    font-size: 20px;
+}
+</style>
